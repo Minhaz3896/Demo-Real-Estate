1323b0b14d6da3674733ccdf04ea4f7cbfa218e2
Minhaz <minhaz3896@gmail.com>
2023-10-19 01:06:54 +0600

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <h1>Find the perfect Place</h1>
+                <p className='Search-text'>Search The laregerst asdome tghsdf lsadkfjbalfk </p>
+                <form>
+                    <div>
+                        <input type='text' placeholder='Search...'/>
+                    </div>
+                    <div className='radio'>
+                        <input type='radio' checked/>
+                        <label>Buy</label>
+                        <input type='radio' checked/>
+                        <label>Rent</label>
+                        <button type='submit'><AiOutlineSearch className='icon'/></button>
+
+                        
+                    </div>
+                </form>
+           
+        </div>
+    </div>
+  )
+}
+
+export default Hero
